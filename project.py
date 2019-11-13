0a34f392368bbe6ca65693da4b95c7fdfc0e0152
porpose <57694508+porpose@users.noreply.github.com>
2019-11-12 21:39:33 -0500

    Add files via upload

---
+++ project.py
@@ -0,0 +1,30 @@
+authorize_personnel_list = []
+authorize_personnel_list.append("quanfeiy")
+authorize_personnel_list.append("zoujinto")
+answer = input("Welcome to criminal risk control system, please enter your id: ")
+if answer == authorize_personnel_list[0] or answer == authorize_personnel_list[1]:
+    print("Access verifcation completed, hello "+ str(answer))
+    verification = "true"
+else:
+    answer = input("Access deny, you have 2 more chances. Enter your id: ")
+    if answer == authorize_personnel_list[0] or answer == authorize_personnel_list[1]:
+        print("Access verifcation completed, hello "+ str(answer))
+        verification = "true"        
+    else:
+        answer = input("Access deny, you have 1 more chance. Enter your id: ")    
+        if answer == authorize_personnel_list[0] or answer == authorize_personnel_list[1]:
+            print("Access verifcation completed, hello "+ str(answer))
+            verification = "true"
+        else:
+            answer = input("Access deny, you are not authorized to use this system.")        
+            from playsound import playsound
+            playsound('FBI_open_up.amr')
+    
+if verification == "true":
+    answer = input("What would you like to do? Enter read or write: ")
+    if answer == "read":
+        my_file = open("project_test.txt", "r") 
+        for line in my_file:
+            print(line)
+    
+    
